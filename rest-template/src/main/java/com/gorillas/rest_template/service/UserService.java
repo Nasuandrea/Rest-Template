989e7f69a0f271bb1855979cdb8857f947e48fec
Nasuandrea <acollazocacho@gmail.com>
2024-08-28 10:00:17 +0200

    dev: update properties, service

--- rest-template/src/main/java/com/gorillas/rest_template/service/UserService.java
+++ rest-template/src/main/java/com/gorillas/rest_template/service/UserService.java
@@ -2,20 +2,27 @@ package com.gorillas.rest_template.service;
 
 import com.gorillas.rest_template.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+    @Value("${spring.external.service.base-url}")
+    private String basePath;
+
     private final RestTemplate restTemplate;
 
 
     public List<UserDTO> getUsers(){
-        return null;
+        UserDTO[] response = restTemplate.getForObject(basePath+"/users",UserDTO[].class);
+        return Arrays.asList(response);
     }
 
     public void saveUser(UserDTO user){}
